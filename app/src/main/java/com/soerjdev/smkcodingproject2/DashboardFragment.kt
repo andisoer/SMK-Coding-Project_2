923c5990064c3098967dd50e4c5baa69b9b54041
Andi <andi502.as@gmail.com>
2020-05-11 09:31:35 +0700

    Bottom Nav, dashboard, graph, info fragment

---
+++ app/src/main/java/com/soerjdev/smkcodingproject2/DashboardFragment.kt
@@ -0,0 +1,27 @@
+package com.soerjdev.smkcodingproject2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DashboardFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DashboardFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+}
