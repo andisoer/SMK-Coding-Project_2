ed4c5ab3dfddd3315ed893500cfab63e3d1985f2
Andi <andi502.as@gmail.com>
2020-05-12 15:48:22 +0700

    Graph with dummy data

--- app/src/main/java/com/soerjdev/smkcodingproject2/IndoGraphFragment.kt
+++ app/src/main/java/com/soerjdev/smkcodingproject2/IndoGraphFragment.kt
@@ -1,10 +1,18 @@
 package com.soerjdev.smkcodingproject2
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+import kotlinx.android.synthetic.main.fragment_indo_graph.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +25,50 @@ class IndoGraphFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_indo_graph, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        setDataToChart()
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setDataToChart() {
+        val entries : ArrayList<PieEntry> = ArrayList()
+
+        val pieColor = arrayListOf(
+            context?.let {
+                ContextCompat.getColor(it, R.color.colorConfirmed)
+            },
+            context?.let {
+                ContextCompat.getColor(it, R.color.colorRecovered)
+            },
+            context?.let {
+                ContextCompat.getColor(it, R.color.colorDeath)
+            }
+        )
+
+        entries.add(PieEntry(15321f, "Positif"))
+        entries.add(PieEntry(4412f, "Sembuh"))
+        entries.add(PieEntry(1540f, "Meninggal"))
+
+        val pieDataSet = PieDataSet(entries, "Statistik Indonesia")
+        pieDataSet.colors = pieColor
+        pieDataSet.valueTextSize = 14f
+        pieDataSet.valueTextColor = Color.WHITE
+
+        val pieData = PieData(pieDataSet)
+
+        val legend = pieChartIndoGraph.legend
+        legend.textSize = 14f
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+
+        pieChartIndoGraph.description.isEnabled = true
+        pieChartIndoGraph.extraTopOffset = -50f
+        pieChartIndoGraph.data = pieData
+        pieChartIndoGraph.invalidate()
     }
 
 }
--- app/src/main/java/com/soerjdev/smkcodingproject2/WorldGraphFragment.kt
+++ app/src/main/java/com/soerjdev/smkcodingproject2/WorldGraphFragment.kt
@@ -1,10 +1,19 @@
 package com.soerjdev.smkcodingproject2
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import kotlinx.android.synthetic.main.fragment_indo_graph.*
+import kotlinx.android.synthetic.main.fragment_world_graph.*
+import java.io.Console
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +26,50 @@ class WorldGraphFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_world_graph, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        setDataToChart()
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setDataToChart() {
+        val entries : ArrayList<PieEntry> = ArrayList()
+
+        val pieColor = arrayListOf(
+            context?.let {
+                ContextCompat.getColor(it, R.color.colorConfirmed)
+            },
+            context?.let {
+                ContextCompat.getColor(it, R.color.colorRecovered)
+            },
+            context?.let {
+                ContextCompat.getColor(it, R.color.colorDeath)
+            }
+        )
+
+        entries.add(PieEntry(5324124f, "Positif"))
+        entries.add(PieEntry(2398321f, "Sembuh"))
+        entries.add(PieEntry(502185f, "Meninggal"))
+
+        val pieDataSet = PieDataSet(entries, "Statistik Dunia")
+        pieDataSet.colors = pieColor
+        pieDataSet.valueTextSize = 14f
+        pieDataSet.valueTextColor = Color.WHITE
+
+        val pieData = PieData(pieDataSet)
+
+        val legend = pieCharWorldGraph.legend
+        legend.textSize = 14f
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+
+        pieCharWorldGraph.description.isEnabled = true
+        pieCharWorldGraph.extraTopOffset = -50f
+        pieCharWorldGraph.data = pieData
+        pieCharWorldGraph.invalidate()
     }
 
 }
