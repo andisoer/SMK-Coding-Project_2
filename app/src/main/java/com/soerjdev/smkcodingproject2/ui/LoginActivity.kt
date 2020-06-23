d471ed0fb632287c8b1f745cf1e8dfcd213c6308
Andi <andi502.as@gmail.com>
2020-06-23 14:52:57 +0700

    [+]facebook auth

--- app/src/main/java/com/soerjdev/smkcodingproject2/ui/LoginActivity.kt
+++ app/src/main/java/com/soerjdev/smkcodingproject2/ui/LoginActivity.kt
@@ -1,30 +1,36 @@
 package com.soerjdev.smkcodingproject2.ui
 
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.soerjdev.smkcodingproject2.MainActivity
 import com.soerjdev.smkcodingproject2.R
-import com.soerjdev.smkcodingproject2.utils.SharedPrefUtil
-import com.soerjdev.smkcodingproject2.utils.showToast
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var sharedPreferences : SharedPreferences
+
     lateinit var gso : GoogleSignInOptions
     lateinit var googleSignInClient: GoogleSignInClient
     lateinit var auth : FirebaseAuth
+
+    lateinit var callbackManager: CallbackManager
 
     val TAG = LoginActivity::class.java.name
 
@@ -36,7 +42,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        /* Setup Google Sign In */
+        /* init Google Sign In */
         gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
@@ -45,6 +51,10 @@ class LoginActivity : AppCompatActivity() {
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         auth = FirebaseAuth.getInstance()
+
+        /* init Facebook Sign I*/
+
+        callbackManager = CallbackManager.Factory.create()
 
 //        sharedPreferences = getSharedPreferences(SharedPrefUtil.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
 //        val isLogin = sharedPreferences.getBoolean(SharedPrefUtil.TAG_IS_LOGIN, false)
@@ -76,13 +86,38 @@ class LoginActivity : AppCompatActivity() {
         }
 
         btnLoginFacebook.setOnClickListener {
-            signInWithFacebook()
+
         }
+
+        btnLoginFacebook.setPermissions("email","public_profile")
+        btnLoginFacebook.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                Log.d(TAG, "fb auth onSucess: $result")
+                signInWithFacebook(result!!.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "fb auth onCancel")
+            }
+
+            override fun onError(error: FacebookException?) {
+                Log.e(TAG, "fb auth onError : "+error?.message)
+            }
+        })
 
     }
 
-    private fun signInWithFacebook() {
-        Log.d(TAG, "sign in with facebook button clicked !")
+    private fun signInWithFacebook(accessToken: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(accessToken.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+                    Log.d(TAG, "fb auth complete")
+                    Log.d(TAG, "User : "+auth.currentUser?.displayName)
+                }else{
+                    Log.e(TAG, "fb credential exception : "+task.exception)
+                }
+            }
     }
 
     private fun signInWithGoogle() {
@@ -126,8 +161,9 @@ class LoginActivity : AppCompatActivity() {
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this){ task ->
                 if(task.isSuccessful){
-                    Log.d(TAG, "Auth complete")
-                    Log.d(TAG, "User : "+auth.currentUser?.displayName)
+//                    Log.d(TAG, "Auth complete")
+//                    Log.d(TAG, "User : "+auth.currentUser?.displayName)
+                    intentToMain()
                 }else{
                     Log.e(TAG, "Failed to authenticate : "+task.exception)
                 }
@@ -144,10 +180,11 @@ class LoginActivity : AppCompatActivity() {
                 val account = task.getResult(ApiException::class.java)
                 Log.d(TAG, "account signed : "+account?.id)
                 authWithGoogle(account?.idToken)
-            }catch (e: ApiException){
-                Log.e(TAG, "error on sign in : "+e.message)
+            }catch (e: ApiException) {
+                Log.e(TAG, "error on sign in : " + e.message)
             }
         }
+        callbackManager.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onStart() {
