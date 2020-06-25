113d770927ac75173fb26cf30d1b25b8059060e3
Andi <andi502.as@gmail.com>
2020-06-25 22:21:05 +0700

    [+]add place history activity,[+]firebase database depedency

---
+++ app/src/main/java/com/soerjdev/smkcodingproject2/PlaceHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.soerjdev.smkcodingproject2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaceHistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place_history)
+    }
+}
